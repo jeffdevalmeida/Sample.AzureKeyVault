9776fc3f47b8d9753241f113c4b4fc46ceb70910
Jefferson Almeida <jefferson.almeida@konia.com.br>
2022-04-25 17:24:48 -0300

    Add project files.

---
+++ Sample.AzureKeyVault/Program.cs
@@ -0,0 +1,10 @@
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+string uri = "https://samplevault.vault.azure.net";
+SecretClient client = new SecretClient(new Uri(uri), new DefaultAzureCredential(true));
+
+string secretName = "auto-secret";
+string secretValue = "Valor super secreto";
+
+await client.SetSecretAsync(secretName, secretValue);
